f6669011c571728b6a37c77a1cf58eb927e79ef8
Len <slowshi@github.com>
2020-08-23 19:02:24 -0700

    updated ci

--- .aofl.js
+++ .aofl.js
@@ -1,5 +1,45 @@
+const fs = require('fs');
 const path = require('path');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+const publicPath = '/';
+const buildPath = path.join(__dirname, 'docs');
 
 module.exports = {
-  name: 'Aofl Starter'
+  name: 'AoflAdmin',
+  build: {
+    path: buildPath,
+    publicPath,
+    eslint: {
+      options: {
+        config: path.join(__dirname, '.eslintrc.js')
+      }
+    },
+    css: {
+      global: {
+        purgeCss: {
+          whitelist: ['loaded']
+        }
+      }
+    },
+    extend: () => {
+      const config = {
+        plugins: [],
+        module: {
+          rules: []
+        }
+      };
+
+      if (process.env.NODE_ENV === 'development') {
+        config.plugins.push(new LiveReloadPlugin());
+      }
+
+      return config;
+    }
+  },
+  devServer: {
+    contentBase: buildPath,
+    publicPath,
+    openPage: publicPath.replace(/^\//, '')
+  }
 };
